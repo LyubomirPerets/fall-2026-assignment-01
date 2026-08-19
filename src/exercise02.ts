546064c3b1f24d9edaf82a16d7f5e539578e7cc7
LyubomirPerets <lyubomirvperets@gmail.com>
2026-08-19 19:57:50 -0400

    found out how to implement custom error

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,4 +1,29 @@
+class CustomError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = 'CustomError';
+    }
+  }
+
 export function transcribeDNA(dna: string): string {
-    
+    let rna = [];
+    for (let i = 0; i < dna.length; i++) {
+      switch (dna[i]) {
+        case 'A':
+          rna.push('U');
+          break;
+        case 'T':
+          rna.push('A');
+          break;
+        case 'C':
+          rna.push('G');
+          break;
+        case 'G':
+          rna.push('C');
+          break;
+        default:
+          throw new CustomError('No Such Nucleotide');
+      }
+    }
   return '';
 }
