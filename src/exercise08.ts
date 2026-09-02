7c1c654bc87b3c1a2530e236f417661b4e5b5056
LyubomirPerets <lyubomirvperets@gmail.com>
2026-09-02 12:16:58 -0400

    8

--- src/exercise08.ts
+++ src/exercise08.ts
@@ -4,10 +4,19 @@ export type EventMap = {
 };
 
 export class SimpleEventEmitter<T extends EventMap> {
+  private listeners: { [K in keyof T]?: Array<(data: T[K]) => void> } = {};
+
   public on<K extends keyof T>(
     eventName: K,
     callback: (data: T[K]) => void,
-  ): void {}
+  ): void {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+   }
+    this.listeners[eventName]!.push(callback);
+  } 
 
-  public emit<K extends keyof T>(eventName: K, data: T[K]): void {}
+  public emit<K extends keyof T>(eventName: K, data: T[K]): void {
+    this.listeners[eventName]?.forEach((callback) => callback(data));
+  }
 }
